0e6eaef04fc40318153f3bec77254b6f41847520
Ratandeep Gupta <ratandeepgupta@gmail.com>
2014-04-05 18:04:23 -0400

    Added a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "RDGS Capital Market"}
   describe "Home page" do
 
     it "should have the content 'RDGSCM'" do
@@ -10,7 +11,7 @@ describe "Static pages" do
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("RDGS Capital Market | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
    describe "Help page" do
@@ -21,7 +22,7 @@ describe "Static pages" do
     end
      it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("RDGS Capital Market | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -33,7 +34,19 @@ describe "Static pages" do
     end
      it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("RDGS Capital Market | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+   describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+     it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
